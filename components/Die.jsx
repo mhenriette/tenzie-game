04941387596a934c6f9645845378abaa6c5223cb
mhenriette <mhenriette00@gnmail.com>
2023-03-06 10:08:23 +0200

    refactor: format code

--- components/Die.jsx
+++ components/Die.jsx
@@ -1,8 +1,8 @@
 
-const Die = ({value, className, id, holdDice}) => {
+const Die = ({ value, className, id, holdDice }) => {
   return <div className={`die-face bg-red-500 ${className}`} onClick={holdDice}>
-  <h2 className='die-num'>{value}</h2>
-</div>;
+    <h2 className='die-num'>{value}</h2>
+  </div>;
 }
 
 export default Die;
